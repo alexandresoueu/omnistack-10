c6c5bab7fc30e0289d530bbb0c513d5ad2fdbf2e
calexandredev <calexandredev@gmail.com>
2020-01-21 20:19:09 -0300

    create layout register and your css

--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,7 @@
-import React, { useState } from 'react'
-
+import React from 'react'
+import './global.css'
+import './App.css'
+import './Sidebar.css'
 /** 
  * Component = Block isolate of HTML, CSS and JS, which don`t interfer of restant of aplication
  * State = informations which a component Dad pass to component Son
@@ -7,17 +9,37 @@ import React, { useState } from 'react'
 */
 
 function App() {
-  const [counter, setCounter] = useState(0)
-
-  function incrementCounter() {
-    setCounter(counter+ 1)
-  }
 
   return (
-    <>
-      <h1>Counter: { counter }</h1>
-      <button onClick={ incrementCounter }>Increment</button>
-    </>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuario do github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+
+      </main>
+    </div>
   )
 }
 
