747d60bd8faaa9101f604f9efd5bfa07f5d942bb
calexandredev <calexandredev@gmail.com>
2020-01-21 15:07:19 -0300

    Compnent and State

--- web/src/App.js
+++ web/src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './Header'
+
+/** 
+ * 
+ * Component = isolate code that won't affect other part of application
+ * State = pass property to component 
+ * Property =
+*/
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header title="LET'S GO" />
+      <Header title="HEY HO" />
+      <Header title="PET BULLET" />
+    </>
+  )
 }
 
 export default App;
